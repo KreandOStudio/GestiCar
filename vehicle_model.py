5276f43753c04e094c19fbb7a27e88491e8a78a8
Ángel Álvarez <kreandostudio@gmail.com>
2019-01-14 00:26:27 +0100

    Añadimos el atributo de tipo de vehiculo para introducir si es Turismo, Furgoneta o Motocicleta.

--- vehicle_model.py
+++ vehicle_model.py
@@ -4,9 +4,10 @@
 
 class Vehicle(object):
 
-    def __init__(self, marca_val, modelo_val):
+    def __init__(self, marca_val, modelo_val, tipo_vehiculo_val):
         self.marca = marca_val
         self.modelo = modelo_val
+        self.tipo_vehiculo = tipo_vehiculo_val
 
     def get_full_vehicle(self):
-        return "Marca: {}; Modelo: {};".format(self.marca, self.modelo)
+        return "Marca: {}; Modelo: {}; Tipo de vehículo: {}".format(self.marca, self.modelo, self.tipo_vehiculo)
